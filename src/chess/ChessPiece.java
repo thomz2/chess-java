d437954332e848be39ad00231e3f4f3377d357c6
thomz2 <thomaz2pinheiro@gmail.com>
2022-08-09 18:20:46 -0300

    Check logic

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -12,11 +12,15 @@ public abstract class ChessPiece extends Piece{
 		super(board);
 		this.color = color;
 	}
-
+	
 	public Color getColor() {
 		return color;
 	}
 	
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
+	}
+
 	protected boolean isThereOpponentPiece(Position position) {
 		ChessPiece p = (ChessPiece)getBoard().piece(position);
 		return p != null && p.getColor() != color;
